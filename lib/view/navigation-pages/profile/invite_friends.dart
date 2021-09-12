123039010fd2c88c5def769bae5703e26cdf016d
yasin459 <yasinnmoosavi@gmail.com>
2021-09-12 14:40:11 +0430

    invite_friends.dart added

--- lib/view/navigation-pages/profile/invite_friends.dart
+++ lib/view/navigation-pages/profile/invite_friends.dart
@@ -1,13 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:rabbito/global/strings/image_strings.dart';
 import 'package:rabbito/view/widgets/custom_container.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:path/path.dart' as Path;
 
 class InviteFriends extends StatelessWidget {
   const InviteFriends({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final RenderObject? box = context.findRenderObject();
     return Card(
       elevation: 10,
       margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
@@ -25,15 +31,32 @@ class InviteFriends extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
             width: double.infinity,
             child: CustomContainer(
-              child: Text(
-                "invite-friends".tr,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
+                child: Text(
+                  "invite-friends".tr,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
                 ),
-              ),
-              onPressed: () {},
-            ),
+                // onPressed: ()=> Share.share('check out my website https://example.com',subject: "please join me!")
+                onPressed: ()  {
+                  // Directory tempDir = await getTemporaryDirectory();
+                  // String tempPath = tempDir.path;
+                  //
+                  // Directory appDocDir =
+                  //     await getApplicationDocumentsDirectory();
+                  // String appDocPath = appDocDir.path;
+                  // print("temp: $tempPath");
+                  // print("app: $appDocPath");
+                  return Share.share(
+                    "Hi my Friend!\n i found this cole game for learning" +
+                        " every language with playing card games!" +
+                        ",use this code to get bonus\n" +
+                        "1241435252\n"+"site info:",
+                    subject: "hey where are you???",
+
+                  );
+                }),
           )
         ],
       ),
